3cf8e625a22b28a558603ae6800e697157bf21ce
madorno3 <110795406+madorno3@users.noreply.github.com>
2024-06-02 12:26:38 -0400

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,1 +1,27 @@
+let button = document.getElementById("submit");
+let div = document.getElementById("div");
+let flavor = document.getElementById("flavor");
+let size = document.getElementById("size");
+let rating = document.getElementById("rating");
+let image = document.getElementById("image");
 
+$('.delete-cupcake').click(deleteCupcake)
+
+async function makeCupcake(e) {
+  e.preventDefault(); 
+  try {
+    const response = await axios.post("/api/cupcakes", {
+      flavor: flavor.value,
+      size: size.value,
+      rating: rating.value,
+      image: image.value
+    });
+    let cupcakeLi = document.createElement("li"); 
+    cupcakeLi.textContent = `${response.data.cupcake.flavor} - Size ${response.data.cupcake.size} - Rating: ${response.data.cupcake.rating} -Image ${response.data.cupcake.image}`; // Set the text content
+    document.querySelector("ul").appendChild(cupcakeLi); 
+  } catch (error) {
+    console.error(error); 
+  }
+}
+
+button.addEventListener("click", makeCupcake);
